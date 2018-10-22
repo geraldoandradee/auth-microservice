fdc659aad9143b4ad3336bc761c2a1a133be3345
Geraldo Andrade <geraldo@geraldoandrade.com>
2018-10-22 00:37:01 -0300

    Fixed template path.

--- servers/oauth2/server.py
+++ servers/oauth2/server.py
@@ -1,45 +1,14 @@
 # -*- coding: utf-8 -*-
-
+import os
 from flask import Blueprint
 from flask_oauthlib.provider import OAuth2Provider
 
-from servers.oauth2.config import configure
 from servers.oauth2.routes import setup_routes
 
-oauth2_server = Blueprint(__name__, 'oauth2_server', template_folder='templates')
-
-configure(oauth2_server)
+oauth2_server = Blueprint(__name__, 'oauth2_server',
+                          template_folder=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates'))
 
 oauth = OAuth2Provider()
 oauth.init_app(oauth2_server)
 
 setup_routes(oauth2_server, oauth)
-# setup_handlers(app, oauth)
-
-
-#
-# @app.route('/oauth/token', methods=['GET', 'POST'])
-# @oauth.token_handler
-# def access_token():
-#     return None
-#
-#
-# @app.route('/oauth/authorize', methods=['GET', 'POST'])
-# @oauth.authorize_handler
-# def authorize(*args, **kwargs):
-#     user = current_user()
-#     if not user:
-#         return redirect('/')
-#     if request.method == 'GET':
-#         client_id = kwargs.get('client_id')
-#         client = Client.query.filter_by(client_id=client_id).first()
-#         kwargs['client'] = client
-#         kwargs['user'] = user
-#         return render_template('authorize.html', **kwargs)
-#
-#     confirm = request.form.get('confirm', 'no')
-#     return confirm == 'yes'
-#
-#
-
-
